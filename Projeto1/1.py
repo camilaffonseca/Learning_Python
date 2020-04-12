b8f46870864b2f0dc18044ab12c9d7dae9972467
Allan Oliveira Miraballes <nallamo2016@gmail.com>
2020-04-12 19:46:18 +0000

    Learning

---
+++ Projeto1/1.py
@@ -0,0 +1,20 @@
+## Comentários
+
+# coding: utf-8
+
+'''
+sajdfipasdjjas
+oashdoi
+asdpoj
+'''
+
+"""
+asdasd
+"""
+
+# Meu comentário
+e = 12 # Minha var
+
+
+
+# Minha var e = 12
---
+++ Projeto1/2.py
@@ -0,0 +1,16 @@
+## Indentação
+
+
+if True:
+    e = 2132
+
+lista = [
+    'test',
+    'Camila',
+    'Allan',
+    'test',
+    'Camila',
+    'Allan',
+    'test',
+    'Camila'
+]
