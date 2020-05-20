f658d82058b16fc3bf346d6a6996cfb2c8599b8a
Camila Fonseca <camilafonsecafp@outlook.com>
2020-05-20 20:35:50 +0000

    Upload de novos exercicios

---
+++ Prática/ex006.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+
+numero = int(input('Digite um número para ver sua tabuada: '))
+
+print(f'''
+{numero} x 1  = {numero * 1}
+{numero} x 2  = {numero * 2}
+{numero} x 3  = {numero * 3}
+{numero} x 4  = {numero * 4}
+{numero} x 5  = {numero * 5}
+{numero} x 6  = {numero * 6}
+{numero} x 7  = {numero * 7}
+{numero} x 8  = {numero * 8}
+{numero} x 9  = {numero * 9}
+{numero} x 10 = {numero * 10}
+''')
---
+++ Prática/ex007.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+
+real = float(input('Digite uma quantidade de dinheiro em R$: '))
+
+print(f'R${real} equivale a US${round((real * 5.68), 2)}')
