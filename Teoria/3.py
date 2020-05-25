0b06d9b7d4b5a82f2e09626ecef1989470e2ade1
Camila Fonseca <camilafonsecafp@outlook.com>
2020-05-25 13:34:15 -0300

    Alterações nos arquivos

--- Teoria/3.py
+++ Teoria/3.py
@@ -1,2 +1,44 @@
-## Variáveis
+# coding: utf-8
+# Estruturas condicionais
 
+
+# mensagem = input('Você > ')
+# while mensagem != 'sair':
+#     if mensagem == 'ola':
+#         print('Robô - Olá também!')
+#     elif mensagem == 'bom dia':
+#         print('Robô - Bom dia para você também!')
+#     elif mensagem == 'tchau':
+#         print('Robô - Que pena que vai embora :(\nTchau!')
+#     else:
+#         print('Robô - Não consigo entender o que você disse :/')
+#     mensagem = input('Você > ')
+
+
+# senha = '12345'
+# senha_gerente = input('Digite a senha do sistema: ')
+# if senha == senha_gerente:
+#     salario = float(input('Qual o salário do funcionário em reais? '))
+#     while True:
+#         print('''
+#         Selecione a opção do aumento correspondetnte:
+#         1 - Aumento de 5%
+#         2 - Aumento de 10%
+#         ''')
+#         opcao = input('Digite a opção > ')
+#         if opcao == '1':
+#             aumento = 5
+#             salario2 = salario + (salario * 5 / 100)
+#             print(f'O salário de R${salario:.2f} com aumento de {aumento} porcento ficaria \
+# no valor de R${salario2:.2f}')
+#             break
+#         elif opcao == '2':
+#             aumento = 10
+#             salario2 = salario + (salario * 10 / 100)
+#             print(f'O salário de R${salario:.2f} com aumento de {aumento} porcento ficaria \
+# no valor de R${salario2:.2f}')
+#             break
+#         else:
+#             print('Opção inválida!')
+# else:
+#     print('Senha incorreta! Saia daqui!')
