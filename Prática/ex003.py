3e39ecf977456d40829c70c98bef733d5158dfa1
Camila Fonseca <camilafonsecafp@outlook.com>
2020-08-18 15:51:23 -0300

    Bug Fixed: ex 003

--- Prática/ex003.py
+++ Prática/ex003.py
@@ -7,7 +7,7 @@ numero = int(input('Digite um número: '))
 
 dobro = numero * 2
 triplo = numero * 3
-raiz = numero ** (1 / 2):.2f
+raiz = numero ** (1 / 2)
 
 print(f'O dobro de {numero} é {dobro} \nO triplo é {triplo} \
 \nE a raiz quadrada é {raiz}')
