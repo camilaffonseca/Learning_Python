35d316b4ea3c5196d7d33183b3ff9a36af76d5c1
Camila Fonseca <camilafonsecafp@outlook.com>
2020-08-18 22:37:18 -0300

    '_'

--- Prática/ex058.py
+++ Prática/ex058.py
@@ -5,3 +5,47 @@ Faça um programa que jogue par ou ímpar com o computador. O jogo
 só será interrompido quando o jogador perder, mostrando o total
 de vitórias consecutivas que ele conquistou no final do jogo.
 '''
+
+
+from random import randint
+from unidecode import unidecode
+
+print('Vamos jogar PAR OU ÍMPAR?')
+
+vitórias_jogador = 0
+
+while True:
+
+    while True:
+        jogador_opção = unidecode(input('\nPar ou ímpar? ').lower())
+        if jogador_opção == 'par' or jogador_opção == 'impar':
+            break
+        else:
+            print('Resposta inválida, digite PAR ou ÍMPAR...')
+
+    while True:
+        try:
+            jogador_número = int(input('Valor: '))
+            break
+        except Exception:
+            print('Resposta inválida, digite um número inteiro...')
+
+    computador_número = randint(0, 10)
+
+    número_final = jogador_número + computador_número
+
+    if número_final % 2 == 0:
+        número_final_verificado = 'par'
+    else:
+        número_final_verificado = 'impar'
+
+    print(f'\nVocê jogou {jogador_número} e eu {computador_número}'
+          f'\n{número_final} é {número_final_verificado}\n')
+
+    if jogador_opção == número_final_verificado:
+        vitórias_jogador += 1
+        print('Você venceu ^^'
+              '\nVamos jogar novamente...')
+    else:
+        print(f'Você perdeu com {vitórias_jogador} vitórias')
+        break
---
+++ Prática/ex059.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+
+'''
+Crie um programa que leia a idade e o sexo de várias pessoas.
+A cada pessoa cadastrada, o programa deverá perguntar se o
+usuário quer ou não continuar. No final, mostre:
+A) quantas pessoas tem mais de 18 anos.
+B) quantos homens foram cadastrados.
+C) quantas mulheres tem menos de 20 anos.
+'''
+
+pessoas_18 = 0
+homens = 0
+mulheres_20 = 0
+opção = 'N'
+
+while True:
+    
+    while True:
+        try:
+            idade = int(input('Idade: '))
+            break
+        except Exception:
+            print('Resposta Inválida! \nDigite um número inteiro...')
+    
+    while True:
+        sexo = input('Sexo: ').upper()[0]
+        
+        if sexo in 'FM':
+            break
+        else:
+            print('Resposta Inválida! \nDigite MASCULINO ou FEMININO...')
+
+    if idade >= 18:
+        pessoas_18 += 1
+    if sexo == 'M':
+        homens += 1
+    if sexo == 'F' and idade < 20:
+        mulheres_20 += 1
+
+    opção = ' '
+    while opção not in 'NS':
+        opção = input('Continuar? ').upper()[0]
+    if opção == 'N':
+        break
+
+print(f'\nPessoas acima de 18 anos: {pessoas_18}'
+      f'\nHomens cadastrados: {homens}'
+      f'\nMulheres abaixo de 20 anos: {mulheres_20}')
+    
+    
+
+
