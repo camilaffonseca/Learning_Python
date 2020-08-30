97e0c130f3348b0511e50419442996dd95ce3b06
Camila Fonseca <camilafonsecafp@outlook.com>
2020-08-30 17:14:45 -0300

    '_'

--- Prática/ex001.py
+++ Prática/ex001.py
@@ -3,15 +3,17 @@
 # Dissecando uma variável
 
 
-variavel = input('Digite algo: ')
+x = input('Digite algo: ')
+if x.isspace() == False:
+    x = x.strip()
 
 print(f'''
-O tipo primitivo de '{variavel}' é {type(variavel)}
-Tem somente espaços? {variavel.isspace()}
-É numérico? {variavel.isnumeric()}
-É alfabético? {variavel.isalpha()}
-É alfanumérico? {variavel.isalnum()}
-Está em maiúsculas? {variavel.isupper()}
-Está em minúsculas? {variavel.islower()}
-Está capitalizado? {variavel.istitle()}
+O tipo primitivo de '{x}' é {type(x)}
+Tem somente espaços? {x.isspace()}
+É numérico? {x.isnumeric()}
+É alfabético? {x.isalpha()}
+É alfanumérico? {x.isalnum()}
+Está em maiúsculas? {x.isupper()}
+Está em minúsculas? {x.islower()}
+Está capitalizado? {x.istitle()}
 ''')
